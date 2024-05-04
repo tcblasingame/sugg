24dfcce559e31975bbc7fa73468bc171df9a1dca
Timothy (Clay) Blasingame <119372400+tcblasingame@users.noreply.github.com>
2024-05-04 04:35:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,34 +20,3 @@ document.addEventListener("DOMContentLoaded", function() {
     var content = document.querySelector(".content");
     
 
-var videoPlayButton,
-	videoWrapper = document.getElementsByClassName('video-wrapper')[0],
-    video = document.getElementsByTagName('video')[0],
-    videoMethods = {
-        renderVideoPlayButton: function() {
-            if (videoWrapper.contains(video)) {
-				this.formatVideoPlayButton()
-                video.classList.add('has-media-controls-hidden')
-                videoPlayButton = document.getElementsByClassName('video-overlay-play-button')[0]
-                videoPlayButton.addEventListener('click', this.hideVideoPlayButton)
-            }
-        },
-
-        formatVideoPlayButton: function() {
-            videoWrapper.insertAdjacentHTML('beforeend', '\
-                <svg class="video-overlay-play-button" viewBox="0 0 200 200" alt="Play video">\
-                    <circle cx="100" cy="100" r="90" fill="none" stroke-width="15" stroke="#000"/>\
-                    <polygon points="70, 55 70, 145 145, 100" fill="#000"/>\
-                </svg>\
-            ')
-        },
-
-        hideVideoPlayButton: function() {
-            video.play()
-            videoPlayButton.classList.add('is-hidden')
-            video.classList.remove('has-media-controls-hidden')
-   //         video.setAttribute('controls', 'controls') //video controls
-        }
-	}
-
-videoMethods.renderVideoPlayButton()
